0998926e73e7bc21977f4eb56d50db8b0f4641b8
Shinryo <157136484+5h1nry0@users.noreply.github.com>
2024-02-29 15:06:15 -0500

    Erase non useful functions

--- script.js
+++ script.js
@@ -17,20 +17,3 @@ function addBookToLibrary () {
 
 const theHobbit  = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'not read yet')
 
-console.log(theHobbit.info())
-
-function Player(name, marker) {
-    this.name = name;
-    this.marker = marker;
-    this.sayName = function() {
-      console.log(this.name)
-    };
-}
-  
-const player1 = new Player('steve', 'X');
-const player2 = new Player('also steve', 'O');
-player1.sayName();
-player2.sayName()
-
-Object.getPrototypeOf(player1) === Player.prototype;
-Object.getPrototypeOf(player2) === Player.prototype;
