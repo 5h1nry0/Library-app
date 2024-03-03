bdd7f6a94aa3447810c2cec4910d73f7b863779f
Shinryo <157136484+5h1nry0@users.noreply.github.com>
2024-03-03 14:21:27 -0500

    Add event listeners and form actions

--- script.js
+++ script.js
@@ -10,10 +10,17 @@ function Book(title,author,pages,read){
     } 
 }
 
-function addBookToLibrary () {
-    
+let addBookForm = document.getElementById("addBookForm");
 
-}
+addBookForm.addEventListener("submit", (e) => {   
+    let title = document.getElementById('title').value
+    let author = document.getElementById('author').value
+    let pages = document.getElementById('pages').value
+    let status = document.getElementById('status').value
+    document.getElementById("submit").submit()
+    myLibrary.push(new Book(title, author, pages, status))
+
+})
 
 const theHobbit  = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'not read yet')
 
